8493e4903129b2a3c09a01a366064ca411a517f1
Kasper van den Berg <kasper@kaspervandenberg.net>
2014-08-27 16:46:04 +0200

    - Documentation for using QuadruplePatternRouter. - How to define routes.

--- src/main/scala/net/kaspervandenberg/akkaRdf/actor/QuadruplePatternRouter.scala
+++ src/main/scala/net/kaspervandenberg/akkaRdf/actor/QuadruplePatternRouter.scala
@@ -24,6 +24,120 @@ import scala.collection.Map
  * one end and then branch through the delta until they end up at the correct 
  * actor downstream.
  *
+ * =Examples=
+ * ==Start Akka==
+ * Start Akka and create a `QuadruplePatternRouter` with routes to 
+ * [[RdfStore_InMemory]] actors:
+ * {{{
+ * scala> import akka.actor.ActorSystem
+ * scala> import akka.actor.ActorDSL._
+ * scala> import net.kaspervandenberg.akkaRdf.actor.RdfStore_InMemory
+ * scala> import net.kaspervandenberg.akkaRdf.actor.QuadruplePatternRouter
+ * scala> import net.kaspervandenberg.akkaRdf.rdf.QuadruplePattern._
+ *
+ * scala> implicit val system = ActorSystem("demoSystem")
+ * scala> implicit val mailbox = inbox()
+ * scala> val routes = QuadruplePatternRouter.createRoutesForEachPattern {
+ *      |     case  (pat, f) => actor(
+ *      |         s"storer_${pat.getSimpleName}") {
+ *      |         new RdfStore_InMemory(f) }
+ *      | }
+ * scala> val patternRouter = actor ("patternRouter") { new QuadruplePatternRouter(routes) }
+ * }}}
+ *
+ * ==Stop Akka==
+ * When finished playing with the `QuadruplePatternRouter` stop Akka as 
+ * follows:
+ * {{{
+ * scala> system.shutdown
+ * }}}
+ * ''Note: if you do not call `system.shutdown` Akka will continue running and 
+ * the scala REPL will not stop.''
+ *
+ * ==Load triples==
+ * Use the example from [[net.kaspervandenberg.akkaRdf.rdf.DSL]] to define some 
+ * triples.  The example below is slightly altered so that it contains
+ * [[net.kaspervandenberg.akkaRdf.rdf.Rdf.NamedResource Rdf.NamedResource]]s 
+ * for the graph and the subject 'bob':
+ * {{{
+ * scala> import net.kaspervandenberg.akkaRdf.rdf.DSL
+ * scala> import net.kaspervandenberg.akkaRdf.rdf.Rdf
+ * scala> object dslDemo extends DSL {
+ *      | definePrefix('ex) := "http://example.org/"
+ *      | definePrefix('rdf) := "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+ *      | definePrefix('foaf) := "http://xml.ns.com/foaf/0.1/"
+ *      | definePrefix('xsd) := "http://www.w3.org/2001/XMLSchema#"
+ *      | definePrefix('wikiData) := "http://www.wikidata.org/wiki/"
+ *      | definePrefix('auxRdf) := "http://example.org/rdf/"
+ *      |
+ *      | val work = 'wikiData :: "Property%3AP800"
+ *      | val isBornOn = 'wikiData :: "Property%3AP569"
+ *      | val monaLisa = 'wikiData :: "Q12418"
+ *      | val leonardoDaVinci = 'wikiData :: "Q762"
+ *      |
+ *      | val graphBobInfo = 'ex :: "bobInfo"
+ *      | val subjBob = 'ex :: "bob"
+ *      |
+ *      | object graph1 extends Graph(graphBobInfo) {
+ *      |   addTriple(subjBob ==> 'foaf :: "knows" ==> 'ex :: "alice")
+ *      |   addTriple(subjBob ==> 'rdf :: "type" ==> 'foaf :: "Person")
+ *      |   addTriple(subjBob ==> isBornOn ==>
+ *      |             Rdf.Literal("1990-07-24", 'xsd ::"date"))
+ *      |   addTriple(subjBob ==> 'auxRdf :: "isIntrestedIn" ==> monaLisa)
+ *      | }
+ *      |
+ *      | object graph2 extends Graph('wikiData :: "Special%3AEntityData/Q12418") {
+ *      |   addTriple(leonardoDaVinci ==> work ==> monaLisa)
+ *      |   addTriple('ex :: "videoLaJoconde&#x00C0;Washington" ==>
+ *      |             'auxRdf :: "about" ==> monaLisa)
+ *      | }
+ *      | }
+ * }}}
+ *
+ * Having defined the graph, we can use the following to retrieve some triples:
+ * {{{
+ * scala> val singleTriple = dslDemo.graph1().head
+ * scala> val setOfTriples = dslDemo.graph1() ++ dslDemo.graph2()
+ * }}}
+ *
+ * ==Interact with `patternRouter`==
+ * Define a pattern to query for any triple in the graph `"bobInfo"` having the 
+ * subject `"bob"`:
+ * {{{
+ * scala> import net.kaspervandenberg.akkaRdf.rdf.QuadruplePattern._
+ * scala> import net.kaspervandenberg.akkaRdf.messages.fipa.Performatives._
+ * scala>
+ * scala> val bobQueryPattern = PatternGS__(dslDemo.graphBobInfo, dslDemo.subjBob)
+ * }}}
+ *
+ * The `QuadruplePatternRouter` forwards the [[Inform]] messages to all actors 
+ * on its routes:
+ * {{{
+ * scala> setOfTriples.foreach { patternRouter ! Inform(_) }
+ * scala> patternRouter ! QueryRef(bobQueryPattern)
+ * }}}
+ * results in 4 `Inform` replies one for each `quadruple` matching `( bobInfo, 
+ * bob, _, _); note that the inform messages come from …:
+ * {{{
+ * scala> mailbox.receive()
+ * res9: Any = Inform(Quadruple(NamedResource(http://example.org/bobInfo),NamedResource(http://example.org/bob),NamedResource(http://www.wikidata.org/wiki/Property%3AP569),Literal(1990-07-24,NamedResource(http://www.w3.org/2001/date))))
+ * 
+ * scala> mailbox.receive()
+ * res10: Any = Inform(Quadruple(NamedResource(http://example.org/bobInfo),NamedResource(http://example.org/bob),NamedResource(http://www.w3.org/1999/02/type),NamedResource(http://xml.ns.com/foaf/0.1/Person)))
+ * 
+ * scala> mailbox.receive()
+ * res11: Any = Inform(Quadruple(NamedResource(http://example.org/bobInfo),NamedResource(http://example.org/bob),NamedResource(http://example.org/rdf/isIntrestedIn),NamedResource(http://www.wikidata.org/wiki/Q12418)))
+ * 
+ * scala> mailbox.receive()
+ * res12: Any = Inform(Quadruple(NamedResource(http://example.org/bobInfo),NamedResource(http://example.org/bob),NamedResource(http://xml.ns.com/foaf/0.1/knows),NamedResource(http://example.org/alice)))
+ * 
+ * scala> mailbox.receive()
+ * java.util.concurrent.TimeoutException: deadline passed
+ * }}}
+ * TODO: Show that the sender is the `PatternGS__ storer` and not 
+ * `patternRouter` 
+ * 
+ *
  * @param routes	the [[Pattern]]-subclasses mapped to the [[Actor]]s to
  * 			forward messages to.
  *
@@ -41,6 +155,11 @@ import scala.collection.Map
  *			scala> // Initialise ActorSystem
  *			scala> implicit val system = ActorSystem("demoSys")
  *			scala>
+ *			scala> val routes = QuadruplePatternRouter.createRoutesForEachPattern {
+ * 			     |     case  (pat, f) => actor(
+ * 			     |         s"storer_${pat.getSimpleName}") {
+ * 			     |         new RdfStore_InMemory(f) }
+ * 			     | }
  *			scala> val routes = Pattern.creationFunctions.mapValues (
  *			     | (f) => actor { new RdfStore_InMemory(f) } )
  *			scala> val patternRouter = actor ("patternRouter") { new QuadruplePatternRouter(routes) }
@@ -59,6 +178,27 @@ extends ActorDSL.Act {
 
 		case Failure(_)				=> ()
 		case _unknown				=> sender ! Failure(_unknown)
+	}
+}
+
+object QuadruplePatternRouter {
+	type PatternSubclass = Class[_ <: Pattern]
+	type ToPatternFunction = (Quadruple) => Pattern
+	type ActorConstructionFunction =
+			(PatternSubclass, ToPatternFunction) => ActorRef
+
+	def createRoutesForEachPattern(
+			actorCreator: ActorConstructionFunction):
+	Map[PatternSubclass, ActorRef] =
+	{
+		val builder = scala.collection.immutable.Map.newBuilder[
+				Class[_ <: Pattern],
+				ActorRef]
+		Pattern.creationFunctions.foreach {
+			case (k: PatternSubclass, v: ToPatternFunction) =>
+				builder += (k -> actorCreator(k,v))
+		}
+		builder.result
 	}
 }
 
